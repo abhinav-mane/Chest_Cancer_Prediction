bcf56fb3bcf0b7f9efde1f122f92fd73d4348f11
Abhinav Mane <maneabh1217@gmail.com>
2024-05-08 20:07:14 +0530

    Data Ingestion Done

---
+++ src/Chest_Cancer_Classification_Project/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,33 @@
+from Chest_Cancer_Classification_Project.config.configuration import ConfigurationManager
+from Chest_Cancer_Classification_Project.components.data_ingestion import DataIngestion
+from Chest_Cancer_Classification_Project import logger
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+
+        except Exception as e:
+            raise e
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
