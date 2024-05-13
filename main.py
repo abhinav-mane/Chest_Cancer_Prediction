1edff811b44b7b311521049cca26816dd976b788
Abhinav Mane <maneabh1217@gmail.com>
2024-05-13 14:17:46 +0530

    prepared base model

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from Chest_Cancer_Classification_Project import logger
 from Chest_Cancer_Classification_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Chest_Cancer_Classification_Project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
